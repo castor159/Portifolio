fc483b65dbc79793c5200b5a603de8c96850b90d
castor159 <leosantossilva159@gmail.com>
2026-01-30 21:11:09 -0400

    adicionando atualização
    Atualização da aula 30.01

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -1,2 +1,17 @@
 const botao = document.getElementById("ThemeToggle")
 console.log(botao)
+
+botao.addEventListener('click', function() {
+    console.log("botao clicado");
+        document.body.classList.toggle('dark-mode')
+});
+
+const nomeInput = document.getElementById("nameInput")
+console.log("nomeInput")
+
+const displayName = document.getElementById("display-name")
+console.log("displayName")
+
+nomeInput.addEventListener('input', function(e){
+    displayName.textContent =e.target.value
+});
